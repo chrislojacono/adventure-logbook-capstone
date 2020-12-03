f7b735776ab56cbd2b4509dc9b993ab0f00d103f
Chris LoJacono <clojacono92@gmail.com>
2020-12-03 11:42:19 -0600

    react router, navbar, and views are in place and working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import firebase from 'firebase/app';
+import { BrowserRouter as Router } from 'react-router-dom';
 import fbConnection from '../../helpers/data/Connection';
+import MyNavbar from '../MyNavbar';
+import Routes from '../../helpers/Routes';
 
 fbConnection();
 
 class App extends React.Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { user } = this.state;
     return (
-      <div className="App">
-        <h2>Home</h2>
+      <div className='App'>
+        <Router>
+          <MyNavbar user={user}/>
+          <Routes user={user} />
+        </Router>
       </div>
     );
   }
---
+++ src/components/Views/ToDo.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ToDoList extends Component {
+  render() {
+    return <h1>To Do List!!</h1>;
+  }
+}
