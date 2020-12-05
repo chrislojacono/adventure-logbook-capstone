b8e18c11503298c9c42ee8717f963d8fe24c42f8
Chris LoJacono <clojacono92@gmail.com>
2020-12-05 15:31:52 -0600

    removed CORS proxy for now

--- src/helpers/data/MountainProjectApi.js
+++ src/helpers/data/MountainProjectApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ApiKeys } from '../ApiKeys';
 
 const getRoutes = (lat, lon, maxDist, maxResults, minDiff, maxDiff) => new Promise((resolve, reject) => {
-  axios.get(`https://cors-anywhere.herokuapp.com/https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${lat}&lon=${lon}&maxDistance=${maxDist}&minDiff=${minDiff}&maxDiff=${maxDiff}&maxResults=${maxResults}&key=${ApiKeys.MountainProjectKey}`).then((response) => {
+  axios.get(`https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${lat}&lon=${lon}&maxDistance=${maxDist}&minDiff=${minDiff}&maxDiff=${maxDiff}&maxResults=${maxResults}&key=${ApiKeys.MountainProjectKey}`).then((response) => {
     resolve(response);
   }).catch((error) => reject(error));
 });
