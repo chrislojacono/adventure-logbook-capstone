933325b1237bb7a21f0d0b498666c2a65483bc31
Chris LoJacono <clojacono92@gmail.com>
2020-12-05 11:22:14 -0600

    todo list is populating with the cards

--- src/components/Views/ToDo.js
+++ src/components/Views/ToDo.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+import { getAllUserClimbs } from '../../helpers/data/ClimbData';
+import ToDoCard from '../Cards/ToDoCard';
+import getUid from '../../helpers/data/AuthData';
 
 export default class ToDoList extends Component {
+  state = {
+    toDoClimbs: [],
+  };
+
+  componentDidMount() {
+    this.loadTheClimbs();
+  }
+
+  loadTheClimbs = () => {
+    const userId = getUid();
+    getAllUserClimbs(userId).then((response) => {
+      this.setState({
+        toDoClimbs: response,
+      });
+    });
+  }
+
   render() {
-    return <h1>To Do List!!</h1>;
+    const { toDoClimbs } = this.state;
+    const renderClimbs = () => toDoClimbs.map((climb) => <ToDoCard routeData={climb} key={climb.id} onUpdate={this.loadTheClimbs} />);
+    return (
+        <>
+        <h1 className="toDoTitle">To-Do List</h1>
+        <div className="d-flex justify-content-center flex-wrap">
+        {toDoClimbs.length
+        && renderClimbs()}
+        </div>
+
+        </>
+    );
   }
 }
